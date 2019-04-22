461354d3d095d16e17c4aacdff5d05d04823c82b
ChocPanda <mattsearle@ymail.com>
2019-04-22 14:22:50 +0100

    Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,11 @@
 module.exports = {
-  env: {
-    'jest/globals': true
-  },
-  settings: {
-    'import/resolver': { "babel-module": {} }
-  },
-  parser: 'babel-eslint',
   extends: [
     'prettier',
-    'airbnb',
-    'plugin:prettier/recommended',
-    'plugin:jest/recommended'],
-  plugins: ['prettier', 'jest', 'import'],
+    'airbnb-base',
+    'plugin:prettier/recommended'],
+  plugins: ['prettier', 'import'],
   rules: {
-    'import/prefer-default-export': 'off',
-    'import/no-extraneous-dependencies': 'off',
     'prettier/prettier': ['error'],
-    'no-use-before-define': 'error',
-    // The following are here because airbnb and prettier don't play nicely together
-    'indent': 'off',
-    'no-confusing-arrow': 'off',
-    'implicit-arrow-linebreak': 'off',
-    'function-paren-newline': 'off',
-    'operator-linebreak': 'off',
-    'object-curly-newline': 'off',
-    'arrow-parens': 'off',
-    'import/named': 'off',
+    'no-use-before-define': 'error'
   }
 };
