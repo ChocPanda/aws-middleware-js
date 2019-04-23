9a16562f98309a2868e4f04a5dc77aee78836c92
Matt Searle <msearle@scottlogic.co.uk>
2019-04-23 17:02:08 +0100

    REplace todo with skipped

--- src/__test__/runner.test.js
+++ src/__test__/runner.test.js
@@ -3,31 +3,29 @@ const test = require('ninos')(require('ava'));
 /**
  * The Wrapped lambda function - Init
  */
-test.todo('Should run the init function', t => t.pass());
-test.todo('Should cache the result of the init function', t => t.pass());
-test.todo('Works without an init function', t => t.pass());
+test.skip('Should run the init function', t => t.pass());
+test.skip('Should cache the result of the init function', t => t.pass());
+test.skip('Works without an init function', t => t.pass());
 
 /**
  * The Wrapped lambda function - Middlewares
  */
-test.todo('Executes pre-execution middlewares', t => t.pass());
-test.todo('Executes handles exceptions from pre-execution middlewares', t => t.pass());
-test.todo('Executes post-execution middlewares', t => t.pass());
-test.todo('Executes handles exceptions from post-execution middlewares', t => t.pass());
-test.todo('Executes error handling middlewares', t => t.pass());
+test.skip('Executes pre-execution middlewares', t => t.pass());
+test.skip('Executes handles exceptions from pre-execution middlewares', t => t.pass());
+test.skip('Executes post-execution middlewares', t => t.pass());
+test.skip('Executes handles exceptions from post-execution middlewares', t => t.pass());
+test.skip('Executes error handling middlewares', t => t.pass());
 
 /**
  * The Wrapped lambda function - Invoke
  */
-test.todo('Proxies lambda callback wrapping with Post-Execution middlewares', t => t.pass());
-test.todo('Proxies returned promise wrapping with Post-Execution middlewares', t => t.pass());
+test.skip('Proxies lambda callback wrapping with Post-Execution middlewares', t => t.pass());
+test.skip('Proxies returned promise wrapping with Post-Execution middlewares', t => t.pass());
 
 /**
  * The Wrapped lambda function - Use
  */
-test.todo('Adds the new middleware before component to the beforeMiddlewares', t => t.pass());
-test.todo('Adds the new middleware after component to the afterMiddlewares', t => t.pass());
-test.todo('Adds the new middleware error component to the errorMiddlewares', t => t.pass());
-test.todo('Adds the all the new middleware components to the correct parts of the lifecycle', t =>
-  t.pass()
-);
+test.skip('Adds the new middleware before component to the beforeMiddlewares', t => t.pass());
+test.skip('Adds the new middleware after component to the afterMiddlewares', t => t.pass());
+test.skip('Adds the new middleware error component to the errorMiddlewares', t => t.pass());
+test.skip('Adds the all the new middleware components to the correct parts of the lifecycle', t => t.pass());
