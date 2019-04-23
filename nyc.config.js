9add80c1a9badf591558d3eb0f6e69b6a57b9f97
Matt Searle <msearle@scottlogic.co.uk>
2019-04-23 11:29:09 +0100

    Add nyc config

---
+++ nyc.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'check-coverage': true,
+  'per-file': true,
+  lines: 0,
+  statements: 0,
+  functions: 0,
+  branches: 0,
+  watermarks: {
+    lines: [80, 95],
+    functions: [80, 95],
+    branches: [80, 95],
+    statements: [80, 95]
+  },
+  include: ['src/**/*.js'],
+  exclude: ['src/**/*.test.js'],
+  reporter: ['lcov', 'text-summary'],
+  cache: true,
+  all: true,
+  'report-dir': './reports'
+};
