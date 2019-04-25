103b0ef769a9006b8bbd93415be9844778fb8f1d
Matt Searle <msearle@scottlogic.co.uk>
2019-04-25 18:31:44 +0100

    Start looking at rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,21 @@
-export default {
-	input: 'src/index.js',
-	output: {
-		file: 'bundle.js',
-		format: 'cjs'
+import { uglify } from 'rollup-plugin-uglify';
+
+export default [
+	{
+		input: 'src/lambda.js',
+		output: {
+			dir: 'dist',
+			file: 'bundle.js',
+			format: 'cjs'
+		}
+	},
+	{
+		input: 'src/lambda.js',
+		plugins: [uglify({ comments: 'all' })],
+		output: {
+			dir: 'dist',
+			file: 'bundle.min.js',
+			format: 'cjs'
+		}
 	}
-};
+];
