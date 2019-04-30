032ca46e838186f820955d124d9e2e82d701500d
Matt Searle <msearle@scottlogic.co.uk>
2019-04-30 11:19:27 +0100

    Add query parameters middleware template

---
+++ src/middlewares/http-query-parser/__test__/middleware.test.js
@@ -0,0 +1,5 @@
+const test = require('ninos')(require('ava'));
+
+test.todo('Query Parameter Parsing Middleware - should parse the request url and create a query parameters object');
+
+test.todo('Query Parameter Parsing Middleware - should add an empty query params object when there are no query parameters');
