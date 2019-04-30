717799623633a287013d36e0c28f406e75a580a7
Matt Searle <msearle@scottlogic.co.uk>
2019-04-30 15:47:27 +0100

    Add license information to dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
+import path from 'path';
+
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import license from 'rollup-plugin-license';
 
 export default [
 	{
@@ -9,6 +12,14 @@ export default [
 			format: 'cjs',
 			preferConst: true
 		},
-		plugins: [resolve(), commonjs()]
+		plugins: [
+			resolve(),
+			commonjs(),
+			license({
+				banner: {
+					file: path.join(__dirname, 'LICENSE')
+				}
+			})
+		]
 	}
 ];
