52c238a2f3dd50c5db37971b6eaf80d1f4210230
Matt Searle <msearle@scottlogic.co.uk>
2019-04-30 11:13:04 +0100

    Create http error handling middleware template

---
+++ src/middlewares/http-error-handler/__test__/middleware.test.js
@@ -0,0 +1,5 @@
+const test = require('ninos')(require('ava'));
+
+test.todo('Http Error Handling Middleware - Should catch http-errors and transform the response')
+
+test.todo('Http Error Handling Middleware - Should catch http-errors and include debug information in NODE_ENV: development')
