9a49c61b269c0bc47f72bf9a7196ce98eb713715
ChocPanda <mattsearle@ymail.com>
2019-05-02 01:26:19 +0100

    ci(semantic-release): Turn off npm publish re #12

--- release.config.js
+++ release.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-	branch: 'master'
+	branch: 'master',
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		[
+			'@semantic-release/npm',
+			{
+				npmPublish: false,
+				tarballDir: 'dist'
+			}
+		],
+		[
+			'@semantic-release/github',
+			{
+				assets: 'dist/*.tgz'
+			}
+		]
+	]
 };
