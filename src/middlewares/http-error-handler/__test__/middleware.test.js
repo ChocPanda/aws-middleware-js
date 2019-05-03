c1d0a46429bfae0c2e395ef36caf2de16a09397b
Matt Searle <msearle@scottlogic.co.uk>
2019-05-04 00:25:33 +0100

    test(error-handler): Add some unit tests for the error handler
    re #14

--- src/middlewares/http-error-handler/__test__/middleware.test.js
+++ src/middlewares/http-error-handler/__test__/middleware.test.js
@@ -1,9 +1,27 @@
 const test = require('ninos')(require('ava'));
+const { createHttpError } = require('../../utils');
+const middleware = require('../middleware');
+
+test('Http Error Handling Middleware - Should handle http-errors by transforming the response', t => {
+	const error = createHttpError(404, `Couldn't find any bugs`);
+	const { before: objUnderTest } = middleware();
+
+	t.snapshot(objUnderTest(undefined, error, 'event', 'context'));
+});
+
+test('Http Error Handling Middleware - Should handle non-http-errors by transforming the response if there is a default status code', t => {
+	const error = new Error(`Couldn't find any bugs`);
+	const { before: objUnderTest } = middleware();
+
+	t.snapshot(objUnderTest(undefined, error, 'event', 'context'));
+});
+
+test.todo('Http Error Handling Middleware - Should not include stack traces in production builds')
 
 test.todo(
-	'Http Error Handling Middleware - Should catch http-errors and transform the response'
+	'Http Error Handling Middleware - Should handle include stack traces in dev builds'
 );
 
 test.todo(
-	'Http Error Handling Middleware - Should catch http-errors and include debug information in NODE_ENV: development'
+	'Http Error Handling Middleware - Should not handle http-errors if not configured to do so'
 );
